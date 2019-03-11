a8060beff5c2df4428deeb7081edb4108ca1b2df
SHiziw <45631851+SHiziw@users.noreply.github.com>
2019-03-11 21:00:00 +0800

    学习类

---
+++ src/test1/Dog.java
@@ -0,0 +1,26 @@
+package test1;
+public class Dog{
+    String name;
+    int age;
+   
+    // 构造方法，没有返回值
+    Dog(String name1, int age1){
+        name = name1;
+        age = age1;
+        System.out.println("感谢主人领养了我");
+    }
+   
+    // 普通方法，必须有返回值
+    void bark(){
+        System.out.println("汪汪，不要过来");
+    }
+ 
+    void hungry(){
+        System.out.println("主人，我饿了");
+    }
+   
+    public static void main(String arg[]){
+        // 创建对象时传递的参数要与构造方法参数列表对应
+        Dog myDog = new Dog("花花", 3);
+    }
+}
---
+++ src/test1/Test.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Test {
+	public static void main(String[] args) {
+StringBuffer str1 = new StringBuffer("strubg");
+str1.append("t8rue");
+System.out.print(str1);
+Dog myDog = new Dog("»¨»¨", 3);
+myDog.bark();
+	}
+	
+}
